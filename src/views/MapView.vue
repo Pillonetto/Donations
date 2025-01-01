8bb59e15dd39c74da7edc392f51150ee78dae364
Gabriel Pillonetto <gabrielpillonetto@gmail.com>
2025-01-01 09:56:37 -0300

    mock map view

---
+++ src/views/MapView.vue
@@ -0,0 +1,9 @@
+<template>
+  <PageLayout>
+    <img :src="map" class="h-full w-full" />
+  </PageLayout>
+</template>
+<script setup lang="ts">
+import PageLayout from '@/components/PageLayout.vue'
+import map from '@/assets/map.png'
+</script>
